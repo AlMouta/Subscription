e180f6a18977b7adcf0b402c6533a478c334f74d
AlMouta <almouta@hotmail.com>
2026-03-26 19:38:44 -0300

    feat(dominio): adiciona as entidades cliente, plano e assinatura bem como a baseentity, enums e valueobject.

---
+++ Subscription.Domain/Entities/Assinatura.cs
@@ -0,0 +1,29 @@
+﻿using Subscription.Domain.Commons;
+using Subscription.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Subscription.Domain.Entities
+{
+    public class Assinatura : BaseEntity
+    {
+        #region Propriedades
+
+        public Guid ClienteId { get; set; }
+        public Guid PlanoId { get; set; }
+        public DateTime DataInicio { get; set; } = DateTime.Now;
+        public DateTime? DataFim { get; set; }
+        public decimal Valor { get; set; }
+        public StatusCliente Status { get; set; } = StatusCliente.Ativo;
+
+        #endregion
+
+        #region Relacionamentos
+
+        public Cliente? Cliente { get; set; }
+        public Plano? Plano { get; set; }
+
+        #endregion
+    }
+}
---
+++ Subscription.Domain/ValuesObjects/Email.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Subscription.Domain.ValuesObjects
+{
+    public sealed class Email
+    {
+        public string? Endereco { get; private set; }
+
+        //Contrutor público para gerar uma instância do Email
+        public Email(string endereco)
+        {
+            if (string.IsNullOrWhiteSpace(endereco))
+                throw new ArgumentException("O email é obrigatório.");
+            if (!IsValid(endereco))
+                throw new ArgumentException("O email é inválido.");
+           
+            Endereco = endereco.ToLower();
+        }
+        private bool IsValid(string email)
+        {
+            var regex = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+            return regex.IsMatch(email);
+        }
+
+        public override string ToString() => Endereco ?? string.Empty;
+    }
+}
