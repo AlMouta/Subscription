76ad2e0519ab109c6d1fd3dc937997ff02667100
AlMouta <almouta@hotmail.com>
2026-04-16 01:58:18 -0300

    feat(api): adiciona os serviços de assinaturas, clientes, planos e instala as documentações do swagger e scalar.

---
+++ Subscription.Api/Controllers/AssinaturasController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Subscription.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssinaturasController : ControllerBase
+    {
+        [HttpPost("criar")]
+        public async Task<IActionResult> CriarAsync()
+        {
+            return StatusCode(501, new { Message = "Não implementado." });
+        }
+
+        [HttpPost("cancelar")]
+        public async Task<IActionResult> CancelarAsync()
+        {
+            return StatusCode(501, new { Message = "Não implementado." });
+        }
+
+        [HttpPost("suspender")]
+        public async Task<IActionResult> SuspenderAsync()
+        {
+            return StatusCode(501, new { Message = "Não implementado." });
+        }
+    }
+}
--- Subscription.Api/Program.cs
+++ Subscription.Api/Program.cs
@@ -1,14 +1,25 @@
+using Scalar.AspNetCore;
 using Subscription.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddRouting(map => map.LowercaseUrls = true);    
+
+builder.Services.AddEndpointsApiExplorer(); //Swagger
+builder.Services.AddSwaggerGen(); //Swagger
+
 
 //Injeção de dependências
 builder.Services.AddInfraStructure(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseSwagger(); //Swagger
+app.UseSwaggerUI(); //Swagger
+
+app.MapScalarApiReference(options => options.WithTheme(ScalarTheme.BluePlanet)); //Scalar
 
 app.MapOpenApi();
 app.UseHttpsRedirection();
